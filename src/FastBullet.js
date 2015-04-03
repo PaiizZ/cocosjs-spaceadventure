26be2076cd01ce00d3a99edac846f54b4887fef0
PaiizZ <paiizzmk@gmail.com>
2015-04-03 11:51:34 +0700

    Clean code for Bullet
    make Bullet to super class
    mame FastBullet and WaveBullet to subclass

--- src/FastBullet.js
+++ src/FastBullet.js
@@ -1,4 +1,4 @@
-var FastBullet = cc.Sprite.extend({
+var FastBullet = Bullet.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/bullet.png' );
@@ -8,10 +8,5 @@ var FastBullet = cc.Sprite.extend({
         var x = this.getPositionX();
         x += 10;
         this.setPositionX( x );
-    },
-
-    randomPosition: function() {
-        var y = 10 + Math.floor( Math.random() * ( screenHeight - 10 ) );
-        this.setPosition( new cc.Point( 100, y ) );
     }
 });
--- src/WaveBullet.js
+++ src/WaveBullet.js
@@ -1,4 +1,4 @@
-var WaveBullet = cc.Sprite.extend({
+var WaveBullet = Bullet.extend({
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/bullet.png' );
@@ -11,10 +11,5 @@ var WaveBullet = cc.Sprite.extend({
 
         this.step += 1;
         this.setPosition( cc.p( pos.x + 5, pos.y + 4 * Math.sin( Math.PI * this.step / this.period ) ) );
-    },
-
-    randomPosition: function() {
-        var y = 10 + Math.floor( Math.random() * ( screenHeight - 10 ) );
-        this.setPosition( new cc.Point( 100, y ) );
     }
 });
